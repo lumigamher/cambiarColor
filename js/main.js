deba25a8da0d61d8dc9470c4ce2e27072fa5fab1
lumigamher <lumigam@hotmail.com>
2024-02-23 21:17:49 -0500

    feat: :sparkles: Se agrega JS

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const boton = document.querySelector("button");
+const color = document.getElementById("color");
+
+function generarColor(){
+    let digitos = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i<6;i++){
+        let indiceAleatorio = Math.floor(Math.random()*16);
+        color += digitos[indiceAleatorio];
+    }
+    return color;
+}
+
+boton.addEventListener("click", function(){
+    let colorAleatorio = generarColor();
+    document.body.style.backgroundColor = colorAleatorio;
+    color.textContent=colorAleatorio;
+});
